43ce085742b73b214dde6b6c4ec06da8baaaa922
Ze Dang <4649tz@gmail.com>
2020-09-11 10:02:16 +0800

    [zedang] feat: finish practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const flatten = (columns) => (columns.map(column => column.name))
+
+const compose = (keys, vals) => keys.reduce((obj, key, idx) => ({...obj, [key]: vals[idx]}), {})
+
+const parseData = (response) => {
+  const keys = flatten(response.column);
+  return response.data.map(vals => compose(keys, vals));
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+  let prev = 0
+  let ans = []
+
+  sections.forEach(section => {
+    ans = [...ans, ...items.slice(prev, section.index), section.content]
+    prev = section.index
+  })
+  ans = [...ans, ...items.slice(prev)]
+
+  return ans
+}
+export {inject}
